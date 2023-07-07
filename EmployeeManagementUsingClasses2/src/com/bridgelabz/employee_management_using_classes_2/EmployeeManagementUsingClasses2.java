866f40def87c2845a47387d1dc81d36b801f3121
Prince Kumar <kumarprince1407@gmail.com>
2023-07-07 11:08:23 +0530

    Added Use Case 8

--- EmployeeManagementUsingClasses2/src/com/bridgelabz/employee_management_using_classes_2/EmployeeManagementUsingClasses2.java
+++ EmployeeManagementUsingClasses2/src/com/bridgelabz/employee_management_using_classes_2/EmployeeManagementUsingClasses2.java
@@ -2,20 +2,46 @@ package com.bridgelabz.employee_management_using_classes_2;
 
 import java.util.Random;
 
+class Company{
+    private final String name;
+    private final int wagePerHour;
+    private final int totalWorkingDays;
+    private final int maxWorkingHours;
+
+    public Company(String name, int wagePerHour, int totalWorkingDays, int maxWorkingHours){
+        this.name=name;
+        this.wagePerHour=wagePerHour;
+        this.totalWorkingDays=totalWorkingDays;
+        this.maxWorkingHours=maxWorkingHours;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getWagePerHour(){
+        return wagePerHour;
+    }
+    public int getTotalWorkingDays(){
+        return totalWorkingDays;
+    }
+
+    public int getMaxWorkingHours() {
+        return maxWorkingHours;
+    }
+}
 public class EmployeeManagementUsingClasses2 {
 
-//UC7
+//UC8
 /*Refactor the Code
-to write a Class
-Method to Compute
-Employee Wage - Use Class Method and Class
+to Compute Employee Wage
+for multiple companies - Note: Each Company has its own
+wage, number of working days
+and working hours per month
+- Use Class Method with function
+parameters instead of Class
 Variables*/
-    public static final int fullDayHrs = 8;
-    public static final int wagePerHour=20;
-    public static final int partTimeHours=4;
-    public static final int totalWorkingDays=20;
-    public static final int maxWorkingHours=100;
-    public static void employeeWage(){
+    private static final int fullDayHrs = 8;
+    private static final int partTimeHours=4;
+    public static int employeeWage(Company company){
         int salary =0;
         int workingDays=0;
         int workingHours=0;
@@ -23,22 +49,22 @@ Variables*/
         int empHrs=0;
         Random random = new Random();
 
-        while (workingDays <=totalWorkingDays && workingHours<=maxWorkingHours){
+        while (workingDays <= company.getTotalWorkingDays() && workingHours<= company.getMaxWorkingHours()){
         int attendance = random.nextInt(3);
         switch (attendance){
             case 0:
-                System.out.println("Employee is absent.");
+                System.out.println("Employee is absent for "+ company.getName());
                 break;
             case 1:
-                System.out.println("Employee is present full-time.");
+                System.out.println("Employee is present full-time for "+ company.getName());
                 empHrs=fullDayHrs;
-                salary=empHrs*wagePerHour;
+                salary=empHrs* company.getWagePerHour();
                 System.out.println("Daily wage: "+ salary);
                 break;
             case 2:
-                System.out.println("Employee is present part-time. ");
+                System.out.println("Employee is present part-time for "+ company.getName());
                 empHrs=partTimeHours;
-                salary=empHrs*wagePerHour;
+                salary=empHrs*company.getWagePerHour();
                 System.out.println("Daily wage: "+ salary);
 
         }
@@ -47,14 +73,19 @@ Variables*/
             workingDays++;
     }
 
-        System.out.println("Monthly salary: "+ monthlySalary);
+        System.out.println("Monthly salary for : "+company.getName()+" is: "+ monthlySalary);
+        return monthlySalary;
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Management using classes 2");
+        Company company1 =new Company("ABC",20,20,100);
+        Company company2 =new Company("PQRS",19,20,101);
 
 
-        employeeWage();
+        employeeWage(company1);
+        System.out.println("------------------------------");
+        employeeWage(company2);
 
     }
 }
