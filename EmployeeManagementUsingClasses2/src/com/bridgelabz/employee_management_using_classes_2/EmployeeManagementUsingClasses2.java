85430cc819cfc4bd01fdec9f89f9674980734392
Prince Kumar <kumarprince1407@gmail.com>
2023-07-07 09:53:31 +0530

    Added Use Case 3

--- EmployeeManagementUsingClasses2/src/com/bridgelabz/employee_management_using_classes_2/EmployeeManagementUsingClasses2.java
+++ EmployeeManagementUsingClasses2/src/com/bridgelabz/employee_management_using_classes_2/EmployeeManagementUsingClasses2.java
@@ -4,21 +4,31 @@ import java.util.Random;
 
 public class EmployeeManagementUsingClasses2 {
 
-//UC2
+//UC3
+/*Add Part time
+Employee & Wage
+- Assume Part time Hour is 8*/
     public static void employeeWage(){
         int salary =0;
         int fullDayHrs=8;
         int wagePerHour=20;
+        int partTimeHours=4;
         Random random = new Random();
-        int attendance = random.nextInt(2);
+        int attendance = random.nextInt(3);
         switch (attendance){
             case 0:
                 System.out.println("Employee is absent.");
                 break;
             case 1:
-                System.out.println("Employee is present.");
+                System.out.println("Employee is present full-time.");
                 salary=fullDayHrs*wagePerHour;
                 System.out.println("Daily wage: "+ salary);
+                break;
+            case 2:
+                System.out.println("Employee is present part-time. ");
+                salary=partTimeHours*wagePerHour;
+                System.out.println("Daily wage: "+ salary);
+
         }
 
     }
