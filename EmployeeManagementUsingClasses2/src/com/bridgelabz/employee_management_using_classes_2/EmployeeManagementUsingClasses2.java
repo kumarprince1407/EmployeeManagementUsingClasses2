c8eab201c3af093f7b89c379ba545f01c5aac6af
Prince Kumar <kumarprince1407@gmail.com>
2023-07-07 09:04:00 +0530

    Added UC1

--- EmployeeManagementUsingClasses2/src/com/bridgelabz/employee_management_using_classes_2/EmployeeManagementUsingClasses2.java
+++ EmployeeManagementUsingClasses2/src/com/bridgelabz/employee_management_using_classes_2/EmployeeManagementUsingClasses2.java
@@ -1,7 +1,22 @@
 package com.bridgelabz.employee_management_using_classes_2;
 
+import java.util.Random;
+
 public class EmployeeManagementUsingClasses2 {
+
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Management using classes 2");
+        //UC1
+        Random random = new Random();
+        int attendance = random.nextInt(2);
+        switch (attendance){
+            case 0:
+                System.out.println("Employee is absent.");
+                break;
+            case 1:
+                System.out.println("Employee is present.");
+        }
+
     }
 }
